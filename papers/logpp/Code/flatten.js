a8eb36fb970f52a8c0ad2f2bbf0fc5df49a1d37b
Mark Marron <marron@microsoft.com>
2018-06-19 18:01:03 -0700

    Writing

---
+++ papers/logpp/Code/flatten.js
@@ -0,0 +1,32 @@
+function addExpandedObject(obj, depth, length) {
+  //if the value is in a cycle
+  if(this.jsonCycleSet.has(obj)) {
+    this.addTagEntry(CycleTag);
+    return;
+  }
+
+  if(depth === 0) {
+    this.addTagEntry(DepthBoundTag);
+    return;
+  }
+
+  //Set processing as true for cycle detection
+  this.jsonCycleSet.add(obj);
+  this.addTagEntry(LParenTag);
+
+  let lengthRemain = length;
+  for (const p in obj) {
+    if (lengthRemain <= 0) {
+      this.addTagEntry(LengthBoundTag);
+      break;
+    }
+    lengthRemain--;
+
+    this.addPropertyEntry(p);
+    this.addGeneralEntry(obj[p], depth - 1, length);
+  }
+
+  //Set processing as false for cycle detection
+  this.jsonCycleSet.delete(obj);
+  this.addTagEntry(RParenTag);
+};
